9e87000d5b4a1bff477a91925b270fae5f48392d
IsmaelLebzo <20017635@student.ltuc.com>
2021-08-01 17:00:17 +0300

    added lab01

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+let firstQ = prompt('What is your name?');
+console.log(typeof firstQ);
+console.log(firstQ);
+let secQ = prompt('How old are you?');
+console.log(typeof secQ);
+console.log(secQ);
+let thirdQ = prompt('What is your hobby?');
+console.log(typeof thirdQ);
+console.log(thirdQ);
+let fourthQ = prompt('What do you do in your free time?');
+console.log(typeof fourthQ);
+console.log(fourthQ);
+alert('Hey, nice to meet you ,'+ firstQ +', you look small for a '+secQ+' years old person, I think '+ thirdQ+' is a great hobby! you '+fourthQ+', thats very good!')
+alert('Hey, nice to meet you, Im Ismael im 23 years old, my hobby is playing football, I like to play games with my friends in my free time!')
+document.write('<h3> The Answare for the First Question is: ' + firstQ + '</h3>')
+document.write('<h3> The Answare for the Seconed Question is: ' + secQ + '</h3>')
+document.write('<h3> The Answare for the Third Question is: ' + thirdQ + '</h3>')
+document.write('<h3> The Answare for the Fourth Question is: ' + fourthQ + '</h3>')
